65bf32a12e7ad5900605d276945047e25f70654f
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-03-15 17:08:04 +0400

    z

--- practise/Spread და Rest/2practise(medium).js
+++ practise/Spread და Rest/2practise(medium).js
@@ -1,1 +1,6 @@
 //?გვაქვს ობიექტი product = { id: 1, name: "ტელეფონი", price: 1000 }. შექმენი ახალი ობიექტი updatedProduct, სადაც Spread ოპერატორით დააკოპირებ ძველ მონაცემებს, შეცვლი price-ს 800-ით და დაამატებ სრულიად ახალ თვისებას: inStock: true.
+
+let product = { id: 1, name: "ტელეფონი", price: 1000 };
+
+let updatedProduct = { ...product, price: 800, inStock: true };
+console.log(updatedProduct);
--- practise/Spread და Rest/3practise(hard).js
+++ practise/Spread და Rest/3practise(hard).js
@@ -1,1 +1,12 @@
 //?შექმენი ფუნქცია assignRole(...users), რომელიც Rest ოპერატორით მიიღებს ნებისმიერი რაოდენობის მომხმარებლის ობიექტს (მაგ: {name: "ანა"}, {name: "ნიკა"}). ფუნქციამ map და Spread ოპერატორების კომბინაციით უნდა დააბრუნოს ახალი მასივი, სადაც თითოეულ ობიექტს დამატებული ექნება თვისება role: "user".
+
+function assignRole(...users) {
+  let newObj = users.map((elm) => {
+    return {
+      ...users.elm,
+      role: "user",
+    };
+  });
+}
+
+console.log(assignRole({ name: "ანა" }, { name: "ნიკა" }));
